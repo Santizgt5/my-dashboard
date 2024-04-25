f60fcc61b637ad7f05475df9ea78148b8e1490a6
Santiago Alvarez Rivera <santiagoalvarive@gmail.com>
2024-04-25 08:15:55 -0500

    First commit

---
+++ src/app/shopping-cart/components/CartCounter.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { useState } from "react";
+
+
+interface Props {
+    value?: number;
+}
+
+
+export const CartCounter = ({value = 0}: Props ) => {
+
+    const [contador, setContador] = useState(value)
+
+    const sumar = () => {
+      setContador(contador + 1)
+    }
+  
+    const restar = () => {
+      setContador(contador - 1)
+    }
+
+    return (
+        <>
+            <span className="text-9xl">{contador}</span>
+
+            <div className="flex">
+                <button
+                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+                    onClick={restar}>
+                    -1
+                </button>
+                <button
+                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+                    onClick={sumar}>
+                    +1
+                </button>
+
+            </div>
+        </>
+    )
+}
