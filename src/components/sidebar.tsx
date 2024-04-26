65b01dd4d3200ff72b55c5e52f4eaa79059abfbe
Santiago Alvarez Rivera <santiagoalvarive@gmail.com>
2024-04-26 17:38:06 -0500

    Add pokemons UI and request

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { IoBrowsersOutline, IoCalculator, IoLogoReact } from 'react-icons/io5'
+import { IoBrowsersOutline, IoCalculator, IoFootball, IoLogoReact } from 'react-icons/io5'
 import { SidebarMenuitem } from './SidebarMenuItem'
 
 const menuItems = [
@@ -14,19 +14,25 @@ const menuItems = [
         icon: <IoCalculator size={40} />,
         title: 'Counter',
         subTitle: 'Contador Client Side'
+    },
+    {
+        path: '/dashboard/pokemons',
+        icon: <IoFootball size={40} />,
+        title: 'Pokemons',
+        subTitle: 'Generación Estática'
     }
 ]
 
 export const Sidebar = () => {
-  return (
+    return (
 
-    <div id="menu" 
-        style={ {width: '400px'} }
-        className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll">
+        <div id="menu"
+            style={{ width: '400px' }}
+            className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 overflow-y-scroll">
             <div id="logo" className="my-4 px-6">
                 <h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
-                    <IoLogoReact className='mr-2'/>
-                    <span>Dash</span> 
+                    <IoLogoReact className='mr-2' />
+                    <span>Dash</span>
                     <span className="text-blue-500">Board</span>.
                 </h1>
                 <p className="text-slate-500 text-sm">Administra tus acciones y actividades</p>
@@ -39,20 +45,20 @@ export const Sidebar = () => {
                             alt="user avatar"
                             width={50}
                             height={50}
-                            />
+                        />
                     </span>
                     <span className="text-sm md:text-base font-bold">
                         Santiago Alvarez
                     </span>
-                    </a>
+                </a>
             </div>
 
-            
+
 
             <div id="nav" className="w-full px-6">
 
-                {menuItems.map( item => (
-                    <SidebarMenuitem key={ item.path }  {...item} />
+                {menuItems.map(item => (
+                    <SidebarMenuitem key={item.path}  {...item} />
                 ))}
 
                 {/* <a href="#" className="w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 hover:bg-white/5 transition ease-linear duration-150">
@@ -66,8 +72,8 @@ export const Sidebar = () => {
                         <span className="text-sm text-slate-500 hidden md:block">Estado local</span>
                     </div>
                 </a> */}
-            
-                </div>
+
             </div>
-  )
+        </div>
+    )
 }
